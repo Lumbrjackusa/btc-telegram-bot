a3da8809c4b2022d4f9a9c01da3ac128a9443346
Lumbrjackusa <boriswork251@gmail.com>
2025-06-17 19:38:50 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+TELEGRAM_TOKEN = "7460766184:AAFOZ5smkrm1b98oi3EfcmGQwqa5xHEvvlo"
+CHAT_ID = "467320456"
+
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.json
+    if data:
+        signal_type = data.get("type", "N/A")
+        price = data.get("price", "N/A")
+        symbol = data.get("symbol", "BTC/USDT")
+        exchange = data.get("exchange", "Binance")
+
+        msg = f"""
+🚨 Señal detectada en {symbol}
+💹 Exchange: {exchange}
+📈 Tipo: {signal_type}
+💰 Precio: {price}
+"""
+
+        requests.post(
+            f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
+            json={"chat_id": CHAT_ID, "text": msg}
+        )
+    return "OK", 200
+
+# Render tomará el puerto desde una variable de entorno
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
